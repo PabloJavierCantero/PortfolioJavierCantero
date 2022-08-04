a96bcb7c3c6cc7c7fec213ccbb41469b2e5a3a47
Pablo Cantero <javicantero2002@gmail.com>
2022-08-04 13:01:53 -0300

    cambio backend
    solucion problema backend

---
+++ PortfolioCantero/BackEnd/Pablo/src/main/java/com/Cantero/Pablo/service/IPersonaService.java
@@ -0,0 +1,17 @@
+
+package com.Cantero.Pablo.service;
+
+import com.Cantero.Pablo.model.Persona;
+import java.util.List;
+
+
+public interface IPersonaService {
+    
+    public List<Persona> verPersonas();
+    public void crearPersona(Persona per);
+    public void borrarPersona(Long id);
+    public Persona buscarPersona(Long id);
+    public void editarPersona(Persona per);
+    
+    
+}
---
+++ PortfolioCantero/BackEnd/Pablo/src/test/java/com/Cantero/Pablo/PabloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Cantero.Pablo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PabloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
